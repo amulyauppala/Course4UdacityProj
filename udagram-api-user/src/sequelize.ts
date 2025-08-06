aabb8556b3e06aab64bee8726d55420ac5dcd9f5
amulyauppala <amulyauppala@gmail.com>
2025-08-06 08:01:14 -0700

    Break api into microservices, add reverse proxy. Was building and working UI and API before breaking up but UI is having issues with Docker.

---
+++ udagram-api-user/src/sequelize.ts
@@ -0,0 +1,25 @@
+import {Sequelize} from 'sequelize-typescript';
+import {config} from './config/config';
+
+
+export const sequelize = new Sequelize({
+  'username': config.username,
+  'password': config.password,
+  'database': config.database,
+  'host': config.host,
+
+  'dialect': config.dialect,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false 
+    }
+  },
+  'storage': ':memory:',
+  dialectOptions: {
+      ssl: {
+        require: true, // Fixes SequelizeConnectionError
+        rejectUnauthorized: false // This line will fix new error with above line only.
+      }
+    },
+});
